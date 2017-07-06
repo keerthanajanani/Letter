45fe3978bcb14f29895e8109e730a5d0ecf387cf
keerthanajanani <keerthijanani66@gmail.com>
2017-07-06 21:46:45 +0530

    Create Letter.java

---
+++ Letter.java
@@ -0,0 +1,20 @@
+# Letter
+import java.io.*;
+import java.util.*;
+public class Letter
+{
+public static void main(String args[])throws IOException
+{
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+char c;
+System.out.println("Enter the Character");
+if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
+{
+System.out.println(c +" is Vowel");
+}
+else
+{
+System.out.println(c +" is Consonant"):
+}
+}
+}
